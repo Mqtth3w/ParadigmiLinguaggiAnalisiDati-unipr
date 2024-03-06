ba505ad7dd3c3b0951c0a56c61f99c362aad99ac
Mqtth3w <119968260+Mqtth3w@users.noreply.github.com>
2024-03-06 16:23:08 +0100

    Add files via upload

--- Exercises/sky_class_iterator.py
+++ Exercises/sky_class_iterator.py
@@ -12,17 +12,13 @@ class Sky:
         return self
 
     def __next__(self):
-        if self._index >= len(self._values):
-            raise StopIteration
-        elem = self._values[self._index]
-        self._index +=1
-        if elem > self._min:
-            self._min = elem
-            return elem
-        else:
-            return self.__next__()
-    
-    
+        for i in range(self._index, len(self._values)):
+            elem = self._values[i]
+            if elem > self._min:
+                self._min = elem
+                self._index = i
+                return elem
+        raise StopIteration
     
 values = list(range(1, 6))
 shuffle(values)  # e.g., [3, 1, 4, 2, 5]
