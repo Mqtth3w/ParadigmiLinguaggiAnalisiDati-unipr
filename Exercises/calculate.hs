aaf20795f7540c31ac37e5bf8a73c5db7220cba2
Mqtth3w <119968260+Mqtth3w@users.noreply.github.com>
2024-03-13 18:50:07 +0100

    Update and rename calculator.hs to calculate.hs

--- Exercises/calculate.hs
+++ Exercises/calculate.hs
@@ -4,6 +4,6 @@ calculate :: (Show z, Fractional z, Eq z) => z -> z -> String -> String
 calculate a b "+" = show a ++ " + " ++ show b ++ " = " ++ show (a+b)
 calculate a b "-" = show a ++ " - " ++ show b ++ " = " ++ show (a-b)
 calculate a b "*" = show a ++ " * " ++ show b ++ " = " ++ show (a*b)
-calculate a 0 "/" = "Error!"
+calculate a 0 "/" = "Error!" -- Haskell is able to handle it alonw
 calculate a b "/" = show a ++ " / " ++ show b ++ " = " ++ show (a/b)
 
