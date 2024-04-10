9324263c5bb5b03cd326d54780e6d796f8b1800b
Mqtth3w <mqtth3w@duck.com>
2024-04-10 19:48:28 +0200

    Add files via upload

--- Exercises/4_10-04-2024/map_and_sum.py
+++ Exercises/4_10-04-2024/map_and_sum.py
@@ -7,13 +7,27 @@ print(multiple_any(40, [3, 5, 7]))
 
 #v1 ok
 tot = 0
-for x in count(1):
-    if multiple_any(x**2, [3, 5, 7]) and x**2 < 5000:
-        #print(x**2)
-        tot += x**2
-    elif x**2 >= 5000:
+for i in count(1):
+    if multiple_any(i**2, [3, 5, 7]) and i**2 < 5000:
+        #print(i**2)
+        tot += i**2
+    elif i**2 >= 5000:
         break
 print(tot)
 
 #v2 too weight to calculate
-print(sum(x**2 for x in count(1) if multiple_any(x**2, [3, 5, 7]) and x**2 < 5000))
+#print(sum(i**2 for i in count(1) if multiple_any(i**2, [3, 5, 7]) and i**2 < 5000))
+
+
+#generator
+def sum_gen(ys:list[int], lim:int):
+    gen = 0
+    for x in count(1):
+        if multiple_any(x**2, ys) and x**2 < lim:
+            yield(x**2)
+            gen += x**2
+        elif x**2 >= lim:
+            break
+    
+for y in sum_gen([3, 5, 7], 5000):
+    print(y)
