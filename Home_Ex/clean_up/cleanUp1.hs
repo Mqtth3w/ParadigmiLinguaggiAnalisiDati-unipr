bcd9e9fdd1bd54f97636f56ba655e7a3e26949ae
Mqtth3w <mqtth3w@duck.com>
2024-05-10 15:18:20 +0200

    Add files via upload

---
+++ Home_Ex/clean_up/cleanUp1.hs
@@ -0,0 +1,24 @@
+
+
+main = do
+    let cols = 4
+    let rows = 3
+    putStrLn $ "Matrix dim (cols*rows): " ++ show cols ++ "*" ++ show rows
+    let initialMatrix = [False | i <- [1..cols*rows]]
+    playCleanUp initialMatrix cols rows
+
+playCleanUp matrix cols rows = do 
+    putStrLn "Matrix: "
+    print matrix
+    putStrLn "Insert the col index to play:"
+    x <- getLine
+    putStrLn "Insert the row index to play: "
+    y <- getLine
+    let i = (read x :: Int) + (read y :: Int) * cols
+    let m1 = purePlay (cols,rows) matrix i
+    playCleanUp m1 cols rows     
+
+purePlay :: (Int, Int) -> [Bool] -> Int -> [Bool]
+purePlay (cols,rows) matrix i = 
+    let delta = [i-1,i+1,i-cols,i+cols] -- [w,e,n,s]
+        in [if i `elem` delta then not z else z  | (i,z) <- zip [0..(cols*rows - 1)] matrix]
