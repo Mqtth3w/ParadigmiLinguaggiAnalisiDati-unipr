c85dc0ff38dfbd3e907989e91889e0b95508f50c
Mqtth3w <mqtth3w@duck.com>
2024-05-24 14:14:12 +0200

    Add files via upload

---
+++ Rust_Rayon_project/base_ex.rs
@@ -0,0 +1,9 @@
+
+use rayon::prelude::*;
+
+fn main() {
+    let numbers: Vec<i32> = (0..100).collect();
+    let sum: i32 = numbers.par_iter().sum();
+    println!("The sum is: {}", sum);
+}
+
---
+++ Rust_Rayon_project/multiple_any_par/src/main.rs
@@ -0,0 +1,37 @@
+use std::time::Instant;
+use rayon::prelude::*;
+
+fn multiple_any(x: i64, ys: &[i64]) -> bool {
+    ys.iter().any(|&k| x % k == 0)
+}
+
+fn multiple_any_par(x: i64, ys: &[i64]) -> bool {
+    ys.par_iter().any(|&k| x % k == 0)
+}
+
+fn main() {
+    let sum_limit = 1000000000000;
+    let multiples = [3, 5, 7];
+    //sequential
+    let mut start_time = Instant::now(); 
+    let sum: i64 = (1..)
+        .map(|x| x * x)
+        .take_while(|&sqx| sqx < sum_limit)
+        .filter(|&sqx| multiple_any(sqx, &multiples))
+        .sum();
+    let mut elapsed_time = start_time.elapsed();
+    println!("Total: {}", sum); 
+    //println!("Sequential time: {:?}", elapsed_time); 
+    println!("Sequential time: {:?}", elapsed_time.as_millis());
+    //parallel (is not really parallel the seq is faster)
+    start_time = Instant::now(); 
+    let sum_par: i64 = (1..)
+        .map(|x| x * x)
+        .take_while(|&sqx| sqx < sum_limit)
+        .filter(|&sqx| multiple_any_par(sqx, &multiples))  
+        .sum();
+    elapsed_time = start_time.elapsed();
+    println!("Total in parallel: {}", sum_par); 
+    //println!("Parallel time: {:?}", elapsed_time); 
+    println!("Sequential time: {:?}", elapsed_time.as_millis());
+}
