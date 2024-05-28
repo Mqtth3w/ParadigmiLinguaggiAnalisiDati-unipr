d2f18ae8097969779b1ee2602ff18950677fa35a
Mqtth3w <mqtth3w@duck.com>
2024-05-28 15:08:32 +0200

    Update main.rs

--- Rust_Rayon_project/multiple_any_par/src/main.rs
+++ Rust_Rayon_project/multiple_any_par/src/main.rs
@@ -7,11 +7,11 @@ fn multiple_any(x: i64, ys: &[i64]) -> bool {
 }
 
 fn main() {
-    let sum_limit = 10000000000;
+    let sum_limit = 5000;
     let multiples = [3, 5, 7];
     //sequential
     let mut start_time = Instant::now(); 
-    let mut sum: i64 = (1..)
+    let sum: i64 = (1..)
         .map(|x| x * x)
         .take_while(|&sqx| sqx < sum_limit)
         .filter(|&sqx| multiple_any(sqx, &multiples))
@@ -19,17 +19,15 @@ fn main() {
     let mut elapsed_time = start_time.elapsed();
     println!("Total: {}", sum); 
     println!("Sequential time: {:?}", elapsed_time); 
-    //println!("Sequential time: {:?}", elapsed_time.as_millis());
     //parallel (is not really parallel the seq is faster)
     start_time = Instant::now(); 
-    sum = (1..)
+    let sum_par: i64 = (1..1000000000000000)
+        .into_par_iter() //not working, not feasible in //
         .map(|x| x * x)
         .take_while(|&sqx| sqx < sum_limit)
         .filter(|&sqx| multiple_any(sqx, &multiples))  
-        .par_iter() // mmm?
         .sum();
     elapsed_time = start_time.elapsed();
-    println!("Total in parallel: {}", sum); 
+    println!("Total in parallel: {}", sum_par); 
     println!("Parallel time: {:?}", elapsed_time); 
-    //println!("Sequential time: {:?}", elapsed_time.as_millis());
 }
