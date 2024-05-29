5fc2e3355585a6e252ca446be79a6b049d76fdbb
Mqtth3w <mqtth3w@duck.com>
2024-05-29 15:36:46 +0200

    Update main.rs

--- Rust_Rayon_project/estimate_pi/src/main.rs
+++ Rust_Rayon_project/estimate_pi/src/main.rs
@@ -37,18 +37,20 @@ fn estimate_pi_par(samples: u64) -> f64 {
 }
 
 fn main() {
-    let samples = 1_000_000;
-    //execute sequential
-    let mut start = Instant::now();
-    let mut pi = estimate_pi_seq(samples);
-    let mut duration = start.elapsed();
-    println!("Estimated Pi (sequential): {}", pi);
-    println!("Time taken: {:?}", duration);
-    //execute parallel
-    start = Instant::now();
-    pi = estimate_pi_par(samples);
-    duration = start.elapsed();
-    println!("Estimated Pi (parallel): {}", pi);
-    println!("Time taken: {:?}", duration);
-
+    let samples = [100, 500_000, 1_000_000, 3_000_000, 5_000_000];
+    for num in samples {
+        println!("Samples: {}", num);
+        //execute sequential
+        let mut start = Instant::now();
+        let mut pi = estimate_pi_seq(num);
+        let mut duration = start.elapsed();
+        println!("Estimated Pi (sequential): {}", pi);
+        println!("Time taken: {:?}", duration);
+        //execute parallel
+        start = Instant::now();
+        pi = estimate_pi_par(num);
+        duration = start.elapsed();
+        println!("Estimated Pi (parallel): {}", pi);
+        println!("Time taken: {:?}", duration);
+    }
 }
